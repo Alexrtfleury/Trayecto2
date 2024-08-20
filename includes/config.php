4ac7ac3bb65b98a7d6ed7a5c1b56448e88676d57
Alexander Tovar <alex07.tovar1999@gmail.com>
2024-08-20 02:12:35 -0400

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+|--------------------------------------------------------------------------
+| OWSA-INV V2
+|--------------------------------------------------------------------------
+| Author: Alexander Tovar
+| Project Name: Alex-IVSS
+| Version: v2
+| facebook Page: https://www.facebook.com/oswapp
+|
+|
+|
+*/
+  define( 'DB_HOST', 'localhost' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'oswa_inv' );        // Set database name
+
+?>
